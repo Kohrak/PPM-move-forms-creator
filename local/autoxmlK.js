1ea1f1112239a86a020afd9157d452c651bd586f
Kohrak <kohrakgk0h@gmail.com>
2018-04-18 15:02:30 +0000

    add auto xml feature for kitchen forms

---
+++ local/autoxmlK.js
@@ -0,0 +1,29 @@
+const parser = require('xml2json'),
+      fs = require('fs'),
+      writeGreen = require("./writeGreen"),
+      writeBuff = require("./writeBuff"),
+      getDate = require("./getDate"),
+      emptyFolder = require("./emptyFolder");
+
+function autoxmlK(input){
+  fs.readFile(input, function(err, outp) {
+    if (err){
+      console.log('oh no');
+    } else {
+      let json = parser.toJson(outp, {object: true});
+      let data = json.residentdata.resident
+      data.forEach((obj) => {
+        obj.name = obj.name + obj.surname;
+        writeGreen(obj, getDate());
+        writeBuff(obj, getDate());
+      })
+      emptyFolder("./input/");
+    }
+  });
+}
+
+module.exports = autoxmlK
+
+
+
+ 
--- routes/autoxmlRoutes.js
+++ routes/autoxmlRoutes.js
@@ -1,18 +1,28 @@
 const fileUpload = require('express-fileupload'),
-      autoxml = require('../local/autoxml');
+      autoxml = require('../local/autoxml'),
+      autoxmlK = require('../local/autoxmlK');
 
 module.exports = (app) => {
-    app.get("/xml", (req, res) => {
-        res.render("xml");
-    })
-    
-    app.post("/xml", (req, res) => {
+
+    app.post("/xml/rooms", (req, res) => {
         let inputfile = req.files.dataxml;
         inputfile.mv('./input/input.xml', (err) => {
             if (err){
                 res.send("oh no");
             } else {
                 autoxml('./input/input.xml');
+                res.redirect("/");
+            }
+        })
+    })
+
+    app.post("/xml/kitchen", (req, res) => {
+        let inputfile = req.files.dataxml;
+        inputfile.mv('./input/input.xml', (err) => {
+            if (err){
+                res.send("oh no");
+            } else {
+                autoxmlK('./input/input.xml');
                 res.redirect("/");
             }
         })
