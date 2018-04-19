aa86bfac9c86034aa2532280c9f289b5a220f2bf
Kohrak <kohrakgk0h@gmail.com>
2018-04-19 10:13:32 +0000

    add download routes for xml templates

--- routes/downloadRoutes.js
+++ routes/downloadRoutes.js
@@ -27,12 +27,32 @@ module.exports = (app) => {
     app.get("/download/exec", zip, (req, res) => {
         res.download("./zip/result.zip", (err) => {
             if (err){
-                res.send("something went wrong")
+                res.send("something went wrong");
             } else {
                 emptyFolder('./zip/');
                 emptyFolder('./out/');
             }
         });
+    })
+    
+    app.get("/download/templates/kitchen", (req, res) => {
+        res.download("./templates/xml/kitchenmap.xml", (err) => {
+            if (err){
+                res.send("can't download the file, try again");
+            } else {
+                console.log("kitchen template downloaded");
+            }
+        })
+    })
+    
+    app.get("/download/templates/room", (req, res) => {
+        res.download("./templates/xml/residentmap.xml", (err) => {
+            if (err){
+                res.send("can't download the file, try again");
+            } else {
+                console.log("room template downloaded");
+            }
+        })
     })
 }
 
