2d26889995494ece9fd3cf7e2e0e4465a373564b
Kohrak <kohrakgk0h@gmail.com>
2018-04-20 12:19:00 +0000

    start to write tests

--- spec/writeBlue.spec.js
+++ spec/writeBlue.spec.js
@@ -2,4 +2,26 @@ const writeBlue = require('../local/writeBlue'),
       XlsxPopulate = require('xlsx-populate'),
       fs = require('fs');
       
-      
+describe("When given a move out input", () => {
+    it("creates a form with the provided input", () => {
+         let info = {
+          "name": "Jack",
+          "date": "Smith",
+          "cluster": "10",
+          "room": "2",
+          "key": "Yes",
+          "car": "Na",
+          "laundry": "Yes",
+          "agentmsg": "No charge"
+        }
+    writeBlue(info, "01/01/2018", './out/', () => {
+        fs.readdir('./out/', (err, items) => {
+            if(err){
+                console.log("something went wrong")
+            } else {
+                expect(items.length).toBe(1);
+            }
+        })   
+    })   
+    })
+})
