be54edf1a4f8d1f9452558259a080c8d9abb31b7
Kohrak <kohrakgk0h@gmail.com>
2018-04-21 15:57:45 +0000

    add test to verify data on writeblue

--- spec/writeBlue.spec.js
+++ spec/writeBlue.spec.js
@@ -3,10 +3,9 @@ const writeBlue = require('../local/writeBlue'),
       fs = require('fs');
       
 describe("When given a move out input", () => {
-    it("creates a form ", (done) => {
+    it("creates a form with the right name using input data", (done) => {
          let info = {
           "name": "Jack",
-          "date": "Smith",
           "cluster": "10",
           "room": "2",
           "key": "Yes",
@@ -20,5 +19,43 @@ describe("When given a move out input", () => {
         fs.unlink('./spec/out/' + files[0]);
         done();
     }); 
-    })
+    });
+    it("creates a form that contains the data passed in the right places", (done) => {
+         let info = {
+          "name": "Jack",
+          "cluster": "10",
+          "room": "2",
+          "key": "Yes",
+          "car": "Na",
+          "laundry": "Yes",
+          "agentmsg": "No charge"
+        }
+    writeBlue(info, "01/01/2018", './spec/out/', () => {
+        XlsxPopulate.fromFileAsync('./spec/out/10.2 Cleaning 01.01.2018.xlsx')
+        .then(workbook => {
+            let p = workbook.sheet('Sheet1');
+            let map = {
+                "name": p.cell('B4').value(),
+                "cluster": p.cell('B6').value(),
+                "room": p.cell('B7').value(),
+                "key": p.cell('B8').value(),
+                "car": p.cell('B9').value(),
+                "laundry": p.cell('B10').value(),
+                "agentmsg": p.cell('B19').value()
+            }
+            for (let prop in map){
+                expect(map[prop].toString()).toEqual(info[prop]);
+            }
+            expect(p.cell('B5').value()).toEqual("01/01/2018");
+            let files = fs.readdirSync('./spec/out/');
+            fs.unlink('./spec/out/' + files[0]);
+            done();
+        })
+        .catch(err => {
+            expect(false).toBe(true);
+            done();
+            })
+    }); 
+    });
+    
 })
